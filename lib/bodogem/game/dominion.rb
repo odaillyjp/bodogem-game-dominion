e773d55a76ca069108e4e17e53b78a029a0202b3
Suguru Odai <ourqwers@gmail.com>
2015-12-18 22:37:33 +0900

    Start game

--- lib/bodogem/game/dominion.rb
+++ lib/bodogem/game/dominion.rb
@@ -1,17 +1,44 @@
+require 'singleton'
+
 module Bodogem
   module Game
-    class Dominion < Base
+    class Dominion
       def self.title
         'ドミニオン'.freeze
       end
 
       def initialize
-        @players = []
-        setting_client
+        @supply = Supply.new
+        @trash = []
+        @player = Player.new('あなた')
+        @turn = 0
       end
 
       def start
+        round_start
+      end
+
+      def round_start
+        @turn += 1
+        Application.client.puts "@#{@player.name} さん: #{@turn}ターン目の開始です。"
+      end
+    end
+
+    class Supply
+    end
+
+    class Player
+      attr_reader :name
+
+      def initialize(name)
+        @name = name
+        @discard_cards = []
+        @stack_cards = []
+        @hand_cards = []
+        @play_cards = []
       end
     end
   end
+
+  Application.packages << Game::Dominion
 end
